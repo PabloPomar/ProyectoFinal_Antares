0cb8e6265fbba7ee8b9c85824fc707e00db50b1d
Pomar Pablo <ciberpablo12@hotmail.com>
2022-02-19 17:12:51 -0300

    Agregado frontend web.

--- FrontendWeb/ProyectoFinal_Antares_FrontEnd_Web/src/app/app-routing.module.ts
+++ FrontendWeb/ProyectoFinal_Antares_FrontEnd_Web/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {ComunComponent} from "./Components/Ejemplos/comun/comun.component";
+import {RouterModule, Routes} from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'comun', component: ComunComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontendWeb/ProyectoFinal_Antares_FrontEnd_Web/src/app/app.module.ts
+++ FrontendWeb/ProyectoFinal_Antares_FrontEnd_Web/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComunComponent } from './Components/Ejemplos/comun/comun.component';
+import {AgGridModule} from "@ag-grid-community/angular";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComunComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
