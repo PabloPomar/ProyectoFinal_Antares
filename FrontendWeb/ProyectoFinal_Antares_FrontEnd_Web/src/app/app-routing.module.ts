c0df227b2bdb4481135efabcd763a58193f5da5d
Pomar Pablo <ciberpablo12@hotmail.com>
2022-03-16 19:41:37 -0300

    Home page.

--- FrontendWeb/ProyectoFinal_Antares_FrontEnd_Web/src/app/app-routing.module.ts
+++ FrontendWeb/ProyectoFinal_Antares_FrontEnd_Web/src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { ProductosGridComponent } from './Components/Productos/productos-grid/pr
 import { ProductosFormComponent } from './Components/Productos/productos-form/productos-form.component';
 import {LoginComponent} from "./Components/Login/login/login.component";
 import {UserRegisterComponent} from "./Components/Login/user-register/user-register.component";
+import {HomeComponent} from "./Components/home/home.component";
 
 const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'comun', component: ComunComponent },
     { path: 'productos', component: ProductosGridComponent },
     { path: 'login', component: LoginComponent },
