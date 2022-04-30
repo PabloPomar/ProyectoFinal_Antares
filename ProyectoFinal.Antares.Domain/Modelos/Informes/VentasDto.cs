332201ef14f7676447152a9a65659ceaf72ee22a
Pomar Pablo <ciberpablo12@hotmail.com>
2022-04-30 14:35:59 -0300

    Agregados Reportes Backend

---
+++ ProyectoFinal.Antares.Domain/Modelos/Informes/VentasDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProyectoFinal.Antares.Domain.Modelos.Informes;
+
+public class VentasDto
+{
+    [JsonPropertyName("SalesYear")]
+    public int SalesYear { get; set; }
+    
+    [JsonPropertyName("SalesMonth")]
+    public int SalesMonth { get; set; }
+    
+    [JsonPropertyName("TotalSales")]
+    public double TotalSales { get; set; }
+    
+    [JsonPropertyName("Completadas")]
+    public int Completadas { get; set; }
+    
+    [JsonPropertyName("NoCompletadas")]
+    public int NoCompletadas { get; set; }
+}
---
+++ ProyectoFinal.Antares.Domain/Servicios/IReporteService.cs
@@ -0,0 +1,12 @@
+﻿using ProyectoFinal.Antares.Domain.Modelos.Informes;
+
+namespace ProyectoFinal.Antares.Domain.Servicios;
+
+public interface IReporteService
+{
+    Task<List<VentasDto>> TestReporteVentas();
+
+    Task<List<PedidosClientesDto>> TestPedidoClientes();
+
+    Task<List<DeliveriesDto>> TestDeliveries();
+}
